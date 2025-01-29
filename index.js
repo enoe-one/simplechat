97b59c7877b7a270eb2b860d468c88cdb67e574e
Enoe_one(Enoe) <Enoe.pro@gmail.com>
2025-01-29 11:11:17 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.send("Connection established");
+});
+
+console.log("WebSocket server started on ws://0.0.0.0:8080");
